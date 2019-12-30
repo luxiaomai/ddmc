607900a1b2cd61bc01ac7d3c6329434adbc53422
舍我其谁 <oncwnuBh7Hb3E0j42SIdbc7kOzfE@git.weixin.qq.com>
2019-12-30 11:25:10 +0800

    first

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  runtimeCompiler: true, // 是否使用包含运行时编译器的 Vue 构建版本
+  publicPath: '',
+  productionSourceMap: false, // 不在production环境使用SourceMap
+  devServer: {
+    // 跨域
+    port: 8088, // 端口号
+    open: false, // 配置自动启动浏览器
+    proxy: {
+      // 配置跨域处理 可以设置多个
+      '/api': {
+        target: 'http://api.7-orange.cn:7300/mock/5def6a2d448e330a1116366e/api/',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
+}
