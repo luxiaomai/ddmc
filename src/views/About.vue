147c59980e5b4aa3d549bc07dba17b26b7cdfe33
舍我其谁 <oncwnuBh7Hb3E0j42SIdbc7kOzfE@git.weixin.qq.com>
2019-12-30 11:36:45 +0800

    git

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,14 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.about {
+  font-size: 15px;
+  height: 100px;
+  width: 100px;
+  h1 {
+    color: #ffa000;
+  }
+}
+</style>
